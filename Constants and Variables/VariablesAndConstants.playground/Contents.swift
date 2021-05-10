373380d651122215f239ae231c9a1e3f453d0485
jihoKim94 <kj941225@gmail.com>
2021-05-10 23:29:09 +0900

    2021-05-10

---
+++ Constants and Variables/VariablesAndConstants.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+
+/* variables 변수(가변)
+ Syntax
+    var variableName = initialValue
+*/
+var msg = "Hello world"
+
+var name = 2018
+var valid = false
+
+var x = 0.0 , y = 0.0 , z = 0.0
+
+    name = 2019
+print(name)
+print(msg)
+
+/*
+ Constants 상수 (불변)
+ Syntax
+    let variableName = initialValue
+ */
+
+let celebrityName = "유재석"
+//celebrityName = "박명수"    상수는 변하지않는값을 뜻하기 때문에 다른값으로 초기화 할 수 없다
+print(celebrityName)
+        
